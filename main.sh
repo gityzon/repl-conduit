a05fc5bb13671d0e609ecd5661fb558a7bb36bb4
gityzon <109445070+gityzon@users.noreply.github.com>
2022-09-18 21:07:55 +0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,13 @@
+#！警告：replit免费版中所有代码都是公开可见的，建议使用edu版部署
+#！RUN之前请将conduit.toml中的server_name改为你的域名
+#！更改完成后删除末行#注释，即可启动Conduit
+
+#下载最新版Conduit
+if [ ! -f "conduit" ];then
+  curl -L https://gitlab.com/famedly/conduit/-/jobs/artifacts/master/raw/conduit-x86_64-unknown-linux-musl?job=build:release:cargo:x86_64-unknown-linux-musl -o conduit
+mkdir -p /home/runner/${REPL_SLUG}/database/
+chmod 777 /home/runner/${REPL_SLUG}/database/
+fi
+
+chmod +x conduit
+#./concuit
